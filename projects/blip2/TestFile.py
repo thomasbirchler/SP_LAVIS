f9ccfee8c82ee94cf16cf720b9f2b02c33849be2
Thomas Euler <bithomas@ethz.ch>
2023-11-21 15:52:03 +0100

    nothing to commit+

--- projects/blip2/TestFile.py
+++ projects/blip2/TestFile.py
@@ -6,23 +6,18 @@ def main_function():
     # Create an array of numbers from 0 to 9
     x = np.arange(10)
 
-<<<<<<< HEAD
-# Save squared number into a file
-with open('squared_numbers.txt', 'w') as file:
-    for i in range(len(x)):
-        file.write(f"{x[i]}**2 = {y[i]}; ")
-=======
     # Create a corresponding array of squares
     y = x ** 2
 
     # Save squared number into a file
     with open('squared_numbers.txt', 'w') as file:
-        for i in range(len(x)):
-            file.write(f"{x[i]}^2 = {y[i]}\n")
+        #for i in range(len(x)):
+        #file.write(f"{x[0]}^2 = {y[0]}\n")
+        file.write("fuck this shit")
 
 
 # Check if this script is the main program
 if __name__ == "__main__":
     # Call the main function
     main_function()
->>>>>>> c6de6a88876bad543671e642615866d5efe6e06b
+
